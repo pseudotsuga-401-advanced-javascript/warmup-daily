cd2b0b7b2fb965e9ff4a544062f75405b242d7e2
Conor McCue <conormccue@gmail.com>
2020-01-13 14:07:57 -0800

    end of 15 mins

---
+++ warmup-01/warmup.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// Array Stuff
+const arrayOfNums = [1,2,3,4,5,6,7,8,9,10];
+
+function forLoop(array){
+  for(let number of array){
+    console.log(number);
+  }
+}
+
+
+function whileLoop (array){
+  let x = 0;
+  while(x < array.length){
+    console.log(array[x]);
+  }
+}
+
+function map (array, callback){
+  let newArray = [];
+  for(let i = 0; i <= array.length; i++){
+    newArray[i] = callback(array[i]);
+  }
+  return newArray;
+}
+
+function filter (array, callback){
+  let newArray = [];
+  for(let i = 0; i <= array.length; i++){
+    if (callback(array[i]) === true){
+      newArray[newArray.length] = array[i];
+    }
+  }
+  return newArray;
+}
+
+function reduce(array, callback){
+  let answer;
+  for(let i = 0; i <= array.length; i++){
+    answer = callback(array[i]);
+  }
+  return answer; 
+}
+
+// Object Stuff
+
+const people = ['Kookla','Fran','Ollie'];
+
+const stuff = {
+  tv: 'huge',
+  radio: 'old',
+  toothbrush: 'frayed',
+  cars: ['Toyota','Mazda']
+}
+
+let state = {};
+
+let newPeople = [];
+const newStuff = {};
+let newState = {};
+
+['Odie', ...people, 'Garfield'] = newPeople;
+{tv, radio, toothbrush, [cars, 'New Car']} = newStuff;
