f5aaabb3653fc732eb541a9b2c5b9e221db58de9
Conor McCue <conormccue@gmail.com>
2020-01-21 16:52:59 -0800

    import object starter code

--- warmup-07/warmup.js
+++ warmup-07/warmup.js
@@ -40,3 +40,17 @@ function reduce(array, callback){
   return answer;
 }
 
+const people = ['Kookla','Fran','Ollie'];
+
+const stuff = {
+  tv: 'huge',
+  radio: 'old',
+  toothbrush: 'frayed',
+  cars: ['Toyota','Mazda']
+}
+
+let state = {};
+
+let newPeople = [];
+const newStuff = {};
+let newState = {};
