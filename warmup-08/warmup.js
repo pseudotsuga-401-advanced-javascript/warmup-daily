343c1cbdf57d4286d84839da210d00735ae1e63d
Conor McCue <conormccue@gmail.com>
2020-01-22 10:26:53 -0800

    finish array stuff

--- warmup-08/warmup.js
+++ warmup-08/warmup.js
@@ -1,2 +1,42 @@
 'use strict';
 
+const arrayOfNums = [1,2,3,4,5,6,7,8,9,10];
+
+function forLoop(array){
+  for(let number of array){
+    console.log(number);
+  }
+}
+
+function whileLoop(array){
+  let counter = 0;
+  while(counter < array.length){
+    console.log(array[counter]);
+    counter++;
+  }
+}
+
+function map(array, callback){
+  let answer = [];
+  for(let i = 0; i <= array.length; i++){
+    answer[i] = callback(array[i]);
+  }
+  return answer;
+}
+
+function filter(array, callback){
+  let answer = [];
+  for(let i = 0; i <= array.length; i++){
+    if(callback(array[i])){answer[answer.length] = array[i]}
+  }
+  return answer;
+}
+
+function reduce(array, callback){
+  let answer;
+  for(let i = 0; i <= array.length; i++){
+    answer += callback(array[i]);
+  }
+  return answer;
+}
+
