e506e3bca33548fe03e7cc7457ec9d99c50c5b1e
Conor McCue <conormccue@gmail.com>
2020-01-23 15:55:08 -0800

    end of array stuff

--- warmup-09/warmup.js
+++ warmup-09/warmup.js
@@ -1,1 +1,42 @@
 'use strict';
+
+let arrayOfNums = [1,2,3,4,5,6,7,8,9,10];
+
+function forLoop(array){
+  for(let number of array){
+    console.log(number);
+  }
+}
+
+function whileLoop(array){
+  let x = 0;
+  while(x < array.length){
+    console.log(array[x]);
+    x++;
+  }
+}
+
+function map(array, cb){
+  let answer = [];
+  for(let element in array){
+    answer[element] = cb(array[element]);
+  }
+  return answer;
+}
+
+function filter(array, cb){
+  let answer = [];
+  for(let element in array){
+    if(cb(array[element])){answer[answer.length] = array[element];}
+  }
+  return answer;
+}
+
+function reduce(array, cb){
+  let answer;
+  for(let element in array){
+    cb(answer, array[element]);
+  }
+  return answer;
+}
+
