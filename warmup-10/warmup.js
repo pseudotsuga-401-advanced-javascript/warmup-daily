fa9b256944adaf78430b5bbed89c9e60d96a9edb
Conor McCue <conormccue@gmail.com>
2020-01-24 15:24:47 -0800

    basic array stuff

--- warmup-10/warmup.js
+++ warmup-10/warmup.js
@@ -1,2 +1,20 @@
 'use strict';
 
+// Arrays
+
+const arrayOfNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+function forLoop(array){
+  for(let number of array){
+    console.log(number);
+  }
+}
+
+function whileLoop(array){
+  let counter = 0;
+  while(counter < array.length){
+    console.log(array[counter]);
+    counter++;
+  }
+}
+
